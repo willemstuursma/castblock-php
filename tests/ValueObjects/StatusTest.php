3a6dfc79ac237aaf955d44e50df87c7053023d5d
Willem Stuursma-Ruwen <willem@stuursma.name>
2021-02-22 22:11:21 +0100

    Paused youtube is not playing

--- tests/ValueObjects/StatusTest.php
+++ tests/ValueObjects/StatusTest.php
@@ -16,8 +16,6 @@ class StatusTest extends TestCase
         $actual = Status::fromGoChromeCastOutput($output);
 
         self::assertSame($youtubeIsPlaying, $actual->isPlayingYoutube());
-
-//        self::assertEquals($actual, $i);
     }
 
     public function dpGoChromeCastOutputStrings(): array
@@ -25,6 +23,7 @@ class StatusTest extends TestCase
         return [
             ["Idle (Backdrop), volume=0.78 muted=false\n", false],
             ["[wX0vTXk08Pk] YouTube (PLAYING), title=\"I bought more C64 SID chips from AliExpress! (And some other chips too)\", artist=\"\", time remaining=591s/2636s, volume=1.00, muted=false\n", true]
+            ["[Zg_bLK5XChA] YouTube (PAUSED), title=\"The 2020 Audi RSQ8 Is a $140,000 Super SUV\", artist=\"\", time remaining=201s/1336s, volume=0.85, muted=false\n", false]
         ];
     }
 }
